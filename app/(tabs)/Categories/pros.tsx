fb4b9dc2131e15aeb5f7ea6a47d5766c38598bda
sunnata710 <Sunnata.ong@gmail.com>
2025-09-21 10:56:21 -0700

    Update pros.tsx

--- app/(tabs)/Categories/pros.tsx
+++ app/(tabs)/Categories/pros.tsx
@@ -11,6 +11,9 @@ export default function Pros() {
 
   const [pros, setPros] = useState([
     // CHANGE THE PROS OVER HERE
+    ["Icon", "Cats provide emotional support", ["Purring decreases hormone related to stress, Stroking a cat slows down breathing, Cats are great companions, Provides routine and sense of responsibility"]],
+    ["Icon", "Cats are independent", ["They don't need to be taken out for walks!, Cats can entertain themselves, Cats sleep quite a bit"]],
+    ["Icon", "Title", ["Bullet Point 1, Bullet Point 2"]],
     ["Icon", "Title", ["Bullet Point 1, Bullet Point 2"]],
   ]);
 
