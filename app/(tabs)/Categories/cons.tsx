8c1e8f7793d26c43fe5a410f5a2041f636571d47
Kyle Lin <linkyle0924@gmail.com>
2025-09-23 01:38:26 -0700

    Add Cards

--- app/(tabs)/Categories/cons.tsx
+++ app/(tabs)/Categories/cons.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, Dimensions, View, StyleSheet } from 'react-native';
+import { Text, ScrollView, Dimensions, View, StyleSheet } from 'react-native';
 
 
 
@@ -17,15 +17,21 @@ export default function Cons() {
 
 
   return (
-    <View style = {styles.container}>
+    <ScrollView style = {styles.container}>
+      <View style = {styles.containerInner}>
 
-      {cons.map((con, index) => (
-            <View key={index}>
-              <Text>{con[1]}: {con[2]}</Text>
+        {cons.map((con, index) => (
+            <View key={index} style = {styles.card}>
+              <Text style = {styles.title}>{con[1]}</Text>
+              {(con[2] as string[]).map((bullet, index) => (
+                <Text key = {index} style = {styles.bullets}>{bullet}</Text>
+              ))}
             </View>
         ))}
+      </View>
 
-    </View>
+
+    </ScrollView>
   );
 }
 
@@ -34,11 +40,34 @@ const screenWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
 
   container: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: 'rgb(154, 182, 212)',
+  },
+
+  containerInner: {
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 20,
   },
+
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    width: .8*screenWidth,
+    gap: 10,
+    borderRadius: 15,
+    borderWidth: 3,
+  },
+
+  title: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+
+  bullets: {
+
+  }
 
 
 
--- app/(tabs)/Categories/pros.tsx
+++ app/(tabs)/Categories/pros.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, Dimensions, View, StyleSheet } from 'react-native';
+import { Text, ScrollView, Dimensions, View, StyleSheet } from 'react-native';
 
 
 
@@ -11,24 +11,31 @@ export default function Pros() {
 
   const [pros, setPros] = useState([
     // CHANGE THE PROS OVER HERE
-    ["Icon", "Cats provide emotional support", ["Purring decreases hormone related to stress, Stroking a cat slows down breathing, Cats are great companions, Provides routine and sense of responsibility"]],
-    ["Icon", "Cats are independent", ["They don't need to be taken out for walks!, Cats can entertain themselves, Cats sleep quite a bit"]],
-    ["Icon", "Title", ["Bullet Point 1, Bullet Point 2"]],
-    ["Icon", "Title", ["Bullet Point 1, Bullet Point 2"]],
+    ["Icon", "Cats provide emotional support", ["Purring decreases hormone related to stress", "Stroking a cat slows down breathing", "Cats are great companions", "Provides routine and sense of responsibility"]],
+    ["Icon", "Cats are independent", ["They don't need to be taken out for walks!", "Cats can entertain themselves", "Cats sleep quite a bit"]],
+    ["Icon", "Title", ["Bullet Point 1", "Bullet Point 2"]],
+    ["Icon", "Title", ["Bullet Point 1", "Bullet Point 2"]],
   ]);
 
 
 
   return (
-    <View style = {styles.container}>
+    <ScrollView style = {styles.container}>
+      <View style = {styles.containerInner}>
+
 
         {pros.map((pro, index) => (
-            <View key={index}>
-              <Text>{pro[1]}: {pro[2]}</Text>
+            <View key={index} style = {styles.card}>
+              <Text style = {styles.title}>{pro[1]}</Text>
+              {(pro[2] as string[]).map((bullet, index) => (
+                <Text key = {index} style = {styles.bullets}>{bullet}</Text>
+              ))}
             </View>
         ))}
+      </View>
 
-    </View>
+
+    </ScrollView>
   );
 }
 
@@ -37,11 +44,34 @@ const screenWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
 
   container: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: 'rgb(154, 182, 212)',
+  },
+
+  containerInner: {
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 20,
   },
+
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    width: .8*screenWidth,
+    gap: 10,
+    borderRadius: 15,
+    borderWidth: 3,
+  },
+
+  title: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+
+  bullets: {
+
+  }
 
 
 
